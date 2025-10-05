136e2c7a4be4bfadbdef37b5ef016709d8ebfe43
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-10-05 16:41:01 +0800

    neo

--- src/main/java/me/kall/hungeroverflow/HungerOverflow.java
+++ src/main/java/me/kall/hungeroverflow/HungerOverflow.java
@@ -1,6 +1,6 @@
 package me.kall.hungeroverflow;
 
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod(HungerOverflow.MOD_ID)
 public final class HungerOverflow {
